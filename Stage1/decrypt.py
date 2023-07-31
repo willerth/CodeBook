99acc4c85674d757eed26738391fa9f1c1c1853b
willerth <a.willerth1@gmail.com>
2023-07-30 22:55:15 -0500

    first commit

---
+++ Stage1/decrypt.py
@@ -0,0 +1,36 @@
+dict = {}
+
+dict['M'] = 'a'
+dict['T'] = 'n'
+dict['N'] = 'd'
+dict['J'] = 't'
+dict['P'] = 'h'
+dict['X'] = 'e'
+dict['B'] = 'i'
+dict['R'] = 's'
+dict['L'] = 'm'
+dict['W'] = 'g'
+dict['H'] = 'k'
+dict['Y'] = 'w'
+dict['C'] = 'o'
+dict['I'] = 'f'
+dict['V'] = 'r'
+dict['D'] = 'v'
+dict['G'] = 'l'
+dict['Q'] = 'p'
+dict['F'] = 'b'
+dict['A'] = 'c'
+dict['S'] = 'p'
+dict['E'] = 'y'
+dict['U'] = 'u'
+dict['K'] = 'q'
+dict['Z'] = 'x'
+dict['O'] = 'z'
+
+file = open('Stage1/encrypted.txt', encoding='utf8')
+message = file.read()
+file.close
+
+for char in message:
+    if char in dict.keys(): print(dict[char], end="")
+    else: print(char, end="")
