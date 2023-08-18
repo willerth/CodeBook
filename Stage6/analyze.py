44bc3b1337735214fb7a58cc5e9239d3ed93b61e
Adam Willerth <a.willerth1@gmail.com>
2023-08-18 14:11:43 -0500

    Made analysis a lil cleaner

--- Stage6/analyze.py
+++ Stage6/analyze.py
@@ -4,8 +4,6 @@ file = open('Stage6/encrypted.txt')
 encrypted = file.read().replace('\n', '')
 file.close()
 
-outputFile = open('Stage6/analysis.txt','w')
+outputFile = 'Stage6/analysis.txt'
 
-generateSnippets("the", encrypted, outputFile)
-
-outputFile.close()
+generateSnippets("the", encrypted, outputFile, snippets=200)
--- Stage6/util.py
+++ Stage6/util.py
@@ -1,9 +1,18 @@
-def generateSnippets(commonWord, message, outputFile):
-    for i in range(len(message) - len(commonWord) + 1):
+#Generates potential keyword snippets given an encrypted message and a common english word, and
+#writes every snippet to a given output file. The three optional arguments dictate how many
+#snippets are generated, and at what interval
+def generateSnippets(commonWord, message, filename, start=0, snippets=1000, increment=1):
+    outputFile = open(filename, 'w')
+
+    end = min(snippets, len(message) - len(commonWord) + 1)
+    for i in range(start, end, increment):
+        outputFile.write(f'{i}: '.rjust(5))
         for j in range(len(commonWord)):
             keyLetter = genKeyLetter(commonWord[j], message[i + j])
             outputFile.write(keyLetter)
-        if(i < len(message) - len(commonWord)): outputFile.write('\n')
+        if i < end - 1 : outputFile.write('\n')
+
+    outputFile.close()
         
 
 
